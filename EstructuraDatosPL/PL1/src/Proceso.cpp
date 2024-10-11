3ab4905a40e74b7cdce6812fe7fa6b9e0a8810b5
Raul <raul.cabrera@edu.uah.es>
2024-10-11 10:58:40 +0200

    Primeros pasos main

--- EstructuraDatosPL/PL1/src/Proceso.cpp
+++ EstructuraDatosPL/PL1/src/Proceso.cpp
@@ -3,7 +3,7 @@
 Proceso::Proceso(int pdi, char nombre[6], bool tipo, bool estado, int prioridad)
 {
 	this->pdi=pdi;
-	strcpy(nombre, nombre);
+	strcpy(this->nombre, nombre);
 	this->tipo=tipo;
 	this->estado=estado;
 	this->prioridad=prioridad;
@@ -13,6 +13,21 @@ void Proceso::setPrioridad(int prio){
 	this->prioridad=(prio + 120);
 	}
 
+int Proceso::getPDI(){
+	return this->pdi;
+	}
+
+int Proceso::getPrioridad(){
+	return this->prioridad;
+	}
+
+bool Proceso::getEstado(){
+	return this->estado;
+	} 
+
+bool Proceso::getTipo(){
+	return this->tipo;
+	}
 
 Proceso::~Proceso()
 {
